ba3db982423d6bd9f434815f2fdb60025146dd41
Scott Garner <scott@j38.net>
2015-03-06 14:23:46 -0500

    Simplified sound playing and cleanup

--- Node/nightsymphony.js
+++ Node/nightsymphony.js
@@ -1,95 +1,47 @@
 var Voice = require("./voice");
 var voices = [
-	new Voice("samples/insects/cicada_1.wav"),
-	new Voice("samples/insects/moth.wav"),
-	new Voice("samples/insects/dragonfly_1.wav"),
-	new Voice("samples/insects/cricket_1.wav"),
-	new Voice("samples/insects/cricket_2.wav"),
-	new Voice("samples/insects/dragonfly_2.wav"),
-	new Voice("samples/insects/mosquitos.wav"),
-	new Voice("samples/insects/cicada_2.wav"),	
+  new Voice("samples/insects/cicada_1.wav"),
+  new Voice("samples/insects/moth.wav"),
+  new Voice("samples/insects/dragonfly_1.wav"),
+  new Voice("samples/insects/cricket_1.wav"),
+  new Voice("samples/insects/cricket_2.wav"),
+  new Voice("samples/insects/dragonfly_2.wav"),
+  new Voice("samples/insects/mosquito.wav"),
+  new Voice("samples/insects/cicada_2.wav"),  
 
-	new Voice("samples/bells/bells.plastic.ff.C5B5001.wav"),
-	new Voice("samples/bells/bells.plastic.ff.C5B5003.wav"),
-	new Voice("samples/bells/bells.plastic.ff.C5B5005.wav"),
-	new Voice("samples/bells/bells.plastic.ff.C5B5006.wav"),
-	new Voice("samples/bells/bells.plastic.ff.C5B5008.wav"),
-	new Voice("samples/bells/bells.plastic.ff.C5B5010.wav"),
-	new Voice("samples/bells/bells.plastic.ff.C5B5012.wav"),
-	new Voice("samples/bells/bells.plastic.ff.C6B6001.wav"),
+  new Voice("samples/bells/bells.plastic.ff.C5B5001.wav"),
+  new Voice("samples/bells/bells.plastic.ff.C5B5003.wav"),
+  new Voice("samples/bells/bells.plastic.ff.C5B5005.wav"),
+  new Voice("samples/bells/bells.plastic.ff.C5B5006.wav"),
+  new Voice("samples/bells/bells.plastic.ff.C5B5008.wav"),
+  new Voice("samples/bells/bells.plastic.ff.C5B5010.wav"),
+  new Voice("samples/bells/bells.plastic.ff.C5B5012.wav"),
+  new Voice("samples/bells/bells.plastic.ff.C6B6001.wav"),
 ];
 
-
-
 var firmata = require('firmata');
-
 var board = new firmata.Board('/dev/cu.usbmodemfd1241',
-	{skipCapabilities: true},
-	function(){
+  {skipCapabilities: true},
+  function(){
 
-		console.log("SUP");
+    console.log("Board connected!");
 
-	firmata.SYSEX_RESPONSE[0xb0] = function(board) {
+    firmata.SYSEX_RESPONSE[0xb0] = function(board) {
 
-			console.log(board.currentBuffer);
+      console.log(board.currentBuffer);
 
-			var id = board.currentBuffer[2];
-			var pin = board.currentBuffer[4];
-			var status = board.currentBuffer[6];
+      var id = board.currentBuffer[2];
+      var pin = board.currentBuffer[4];
+      var status = board.currentBuffer[6];
 
-			
-			voice = voices[id];
+      voice = voices[id];
 
-			if (status) {
-				voice.awake();
-			} else {
-				voice.sleep();
-			}
+      if (status) {
+        voice.awake();
+      } else {
+        voice.sleep();
+      }
 
+    }
 
-	}
-
-	});  
-
-
-/*
-
-var ArduinoFirmata = require('arduino-firmata');
-var arduino = new ArduinoFirmata().connect();
-
-arduino.on('connect', function(){
-  console.log("connect!! "+arduino.serialport_name);
-  console.log("board version: "+arduino.boardVersion);
-
-  arduino.sysex(0x01, [13, 5, 2]); 
-
-});
-
-arduino.on('sysex', function(e){
-
-  console.log("command : " + e.command);
-  console.log("data    : " + JSON.stringify(e.data));
-
-	switch (e.command) {
-		case 0xb0:
-
-			var id = e.data[0];
-			var pin = e.data[2];
-			var status = e.data[4];
-
-			console.log(e);
-			
-			voice = voices[id];
-
-			if (status) {
-				voice.awake();
-			} else {
-				voice.sleep();
-			}
-
-		break;
-	}
-
-
-});
-*/
+  });  
--- Node/voice.js
+++ Node/voice.js
@@ -1,37 +1,19 @@
 "use strict";
 
-var T = require("timbre");
+var play = require('play');
 
 function Voice (filename) {
-  //this.readPort = readPort;
   this.awoken = false;
-  this.singing = false;
-  this.sample = T("audio").loadthis(filename, function() {
-    //this.set({bang: false})
-    //this.play();
-  });
- }
+  this.sample = filename;
+}
 
 Voice.prototype.awake = function() {
   this.awoken = true;
-  console.log("WAKE" + this.readPort);
-
-  var voice = this;
-
-  if (!this.singing) {
-    this.singing = true;
-      this.sample.clone().play().on("ended", function() {
-        voice.singing = false;
-        this.pause();
-        console.log("Played");
-      });
-  }
-
+  play.sound(this.sample, function(){});
 };
 
 Voice.prototype.sleep = function() {
   this.awoken = false; 
-  console.log("SLEEP");
 };
 
 module.exports = Voice;
