aa0c821a0698a879b58a9817cb8ca53248057282
Scott Garner <scott@j38.net>
2015-05-29 17:09:09 -0400

    Better board identification. Fixed voice ids

--- Python/nightsymphony.py
+++ Python/nightsymphony.py
@@ -17,24 +17,24 @@ pygame.mixer.set_num_channels(16);
 # 
 
 class Voice():
-	def __init__(self, id, samplePath):
-		self.samplePath = samplePath
-		self.channel = pygame.mixer.Channel(id);
-		self.sample = pygame.mixer.Sound(samplePath);
-	def play(self):
-		self.channel.play(self.sample);
-	def isPlaying(self):
-		return self.channel.get_busy();
+  def __init__(self, id, samplePath):
+    self.samplePath = samplePath
+    self.channel = pygame.mixer.Channel(id);
+    self.sample = pygame.mixer.Sound(samplePath);
+  def play(self):
+    self.channel.play(self.sample);
+  def isPlaying(self):
+    return self.channel.get_busy();
 
 voices = {}
 
 voices[(0,0)] = Voice(0, 'samples/bells/bells.plastic.ff.C5B5001.wav')
-voices[(1,0)] = Voice(0, 'samples/bells/bells.plastic.ff.C5B5002.wav')
-voices[(2,0)] = Voice(0, 'samples/bells/bells.plastic.ff.C5B5003.wav')
-voices[(3,0)] = Voice(0, 'samples/bells/bells.plastic.ff.C5B5004.wav')
-voices[(4,0)] = Voice(0, 'samples/bells/bells.plastic.ff.C5B5005.wav')
-voices[(5,0)] = Voice(0, 'samples/bells/bells.plastic.ff.C5B5006.wav')
-voices[(6,0)] = Voice(0, 'samples/bells/bells.plastic.ff.C5B5007.wav')
+voices[(1,0)] = Voice(1, 'samples/bells/bells.plastic.ff.C5B5002.wav')
+voices[(2,0)] = Voice(2, 'samples/bells/bells.plastic.ff.C5B5003.wav')
+voices[(3,0)] = Voice(3, 'samples/bells/bells.plastic.ff.C5B5004.wav')
+voices[(4,0)] = Voice(4, 'samples/bells/bells.plastic.ff.C5B5005.wav')
+voices[(5,0)] = Voice(5, 'samples/bells/bells.plastic.ff.C5B5006.wav')
+voices[(6,0)] = Voice(6, 'samples/bells/bells.plastic.ff.C5B5007.wav')
 
 #
 # Board Containers
@@ -48,34 +48,45 @@ boards = list()
 # 
 
 class CustomBoard(pyfirmata.Board):
-	def __init__(self, port, layout=None, baudrate=57600, name=None, timeout=None):
-		self.sp = serial.Serial(port, baudrate, timeout=timeout)
-		self.name = name
-		self.isMoon = False;
-		if not self.name:
-			self.name = port		
+  def __init__(self, port, layout=None, baudrate=57600, name=None, timeout=None):
+    self.sp = serial.Serial(port, baudrate, timeout=timeout)
+    self.name = name
+    self.isMoon = False;
+    if not self.name:
+      self.name = port    
 
-	def _handle_identify(self, *data):
-		address = data[0];
-		print "Board Identified: " + str(address)
-		print "On Port: " + str(self.sp.port)
+    self._command_handlers = {}
 
-		if address == 16:
-			print "Moon detected"
-			self.isMoon = True;
+    self.add_cmd_handler(0xb0, self._handle_trigger)
+    self.add_cmd_handler(0xa0, self._handle_identify)
+    self.add_cmd_handler(0xa2, self._handle_echo)     
 
-	def _handle_echo(self, *data):
-		print "ECHO"
-		print data
+    def __str__(self):
+        return "Board {0.name} on {0.sp.port}".format(self)
 
-	def _handle_trigger(self, *data):
-		address = data[0];
-		id = data[2];
-		pin = data[4];
-		status = data[6];
-		print data
-		if (status == 1 and (address,id) in voices):
-			voices[address,id].play();
+  def _handle_identify(self, *data):
+    address = data[0];
+    print "Board Identified: " + str(address)
+    print self
+
+    if address == 16:
+      print "Moon detected"
+      self.isMoon = True;
+
+  def _handle_echo(self, *data):
+    print "ECHO"
+    print data
+
+  def _handle_trigger(self, *data):
+    print self
+
+    address = data[0];
+    id = data[2];
+    pin = data[4];
+    status = data[6];
+    print data
+    if (status == 1 and (address,id) in voices):
+      voices[address,id].play();
 
 #
 # Board Detection
@@ -85,12 +96,10 @@ print "Detecting Boards"
 
 ports = glob.glob('/dev/ttyACM*') + glob.glob('/dev/cu.usbmodem*')
 for port in ports:
-	print "New Board: " + port
-	newBoard = CustomBoard(port, None)
-	newBoard.add_cmd_handler(0xb0, newBoard._handle_trigger)
-	newBoard.add_cmd_handler(0xa0, newBoard._handle_identify)
-	newBoard.add_cmd_handler(0xa2, newBoard._handle_echo)
-	boards.append(newBoard);
+  newBoard = CustomBoard(port, None)
+  print newBoard
+
+  boards.append(newBoard);
 
 #
 # Main Loop
@@ -101,32 +110,32 @@ lastActiveVoiceCount = 0
 
 while True:
 
-	# Process Voices
+  # Process Voices
 
-	activeVoiceCount = 0
+  activeVoiceCount = 0
 
-	for key, voice in voices.iteritems():
-		if voice.isPlaying():
-			activeVoiceCount += 1
+  for key, voice in voices.iteritems():
+    if voice.isPlaying():
+      activeVoiceCount += 1
 
-	# Process Board Data
+  # Process Board Data
 
-	for board in boards:
+  for board in boards:
 
-		# Process input
+    # Process input
 
-		while board.bytes_available():
-			board.iterate();
+    while board.bytes_available():
+      board.iterate();
 
-		# Handle the moon
+    # Handle the moon
 
-		if board.isMoon:
-			if (activeVoiceCount != lastActiveVoiceCount):
-				dataOut = bytearray([activeVoiceCount, len(voices)])
-				boards[0].send_sysex(0xa1, dataOut)			
-		
-		# Quick sleep
+    if board.isMoon:
+      if (activeVoiceCount != lastActiveVoiceCount):
+        dataOut = bytearray([activeVoiceCount, len(voices)])
+        boards[0].send_sysex(0xa1, dataOut)     
+    
+    # Quick sleep
 
-		time.sleep(0.001);	
+    time.sleep(0.001);  
 
-	lastActiveVoiceCount = activeVoiceCount
+  lastActiveVoiceCount = activeVoiceCount
